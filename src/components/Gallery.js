130e777b42136d65f5e1df663302e928c8493ba1
Corey N <coreynotice@yahoo.com>
2023-02-27 21:26:10 -0500

    working

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,9 +4,9 @@ import GalleryItem from '../GalleryItem'
 
 
 
-function Gallery() {
+function Gallery(props) {
   const data= useContext(DataContext)
-  const display =data.map((item,index)=>{
+  const display =props.data.map((item,index)=>{
   return (
       <GalleryItem item={item} key={index}/>
       
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,40 @@
 import { useContext } from 'react'
 import { SearchContext } from '../context/SearchContext'
+import { useState } from 'react'
 
-function SearchBar() {
+
+
+function SearchBar(props) {
+    let [searchTerm, setSearchTerm] = useState('')
     const {term, handleSearch} = useContext(SearchContext)
 
     return (
-        <form>
-            <input ref={term} type="text" placeholder="Search Here" />
-            <button onClick={(e) => handleSearch(e, term.current.value)}>Submit</button>
+        <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+
+            <input type="text" placeholder="Enter a search term here" onChange={
+                (e) => setSearchTerm(e.target.value)
+            } />
+
+            <input type="submit" />
+
         </form>
     )
 }
 
-
 export default SearchBar
+
+
+
+// function SearchBar(props) {
+//     const {term, handleSearch} = useContext(SearchContext)
+// console.log(props)
+//     return (
+//         <form>
+//             <input ref={term} type="text" placeholder="Search Here" />
+//             <button onClick={(e) => handleSearch(e, term.current.value)}>Submit</button>
+//         </form>
+//     )
+// }
+
+
+// export default SearchBar
