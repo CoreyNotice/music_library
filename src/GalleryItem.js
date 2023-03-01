391c346793e9232555f3c340d9c45a1c0258e84f
Corey N <coreynotice@yahoo.com>
2023-03-01 13:29:18 -0500

    done with touble, not loading api info

--- src/GalleryItem.js
+++ src/GalleryItem.js
@@ -1,49 +1,50 @@
-import React,{useState} from 'react'
+import { useState } from 'react'
 
-function GalleryItem(props) {
-  let[view,setView]=useState(false)
-  const simpleStyle = {
-    'width': '25vw',
-    'height': '20vh',
-    'border': '1px solid black',
-    'margin': '2px'
-}
+const GalleryItem = (props) => {
+    let [view, setView] = useState(false)
 
-const detailStyle = {
-    'width': '80vw',
-    'height': '20vh',
-    'border': '1px solid black',
-    'margin': '2px',
-    'backgroundImage': `url(${props.item.artworkUrl100})`,
-    'backgroundRepeat': 'no-repeat',
-    'backgroundSize': 'cover',
-    'color': 'yellow'
-}
+    const simpleView = () => {
+        return (
+            <div style={{
+                'width': '25vw',
+                'height': '20vh',
+                'border': '1px solid black',
+                'margin' : '2px',
+                'position': 'relative'
+            }}>
+                <h3>{props.item.trackName}</h3>
+                <h4>{props.item.collectionName}</h4>
+            </div>
+        )
+    }
 
+    const detailView = () => {
+        return (
+            <div style={{
+                'width': '80vw',
+                'height': '20vh',
+                'border': '1px solid black',
+                'margin' : '2px',
+                'position': 'relative',
+                'backgroundImage': `url(${props.item.artworkUrl100})`,
+                'backgroundRepeat': 'no-repeat',
+                'backgroundSize': 'cover',
+                'color': 'white'
+            }}>
+                <h2>{props.item.trackName}</h2>
+                <h3>{props.item.collectionName}</h3>
+                <h4>{props.item.primaryGenreName}</h4>
+                <h4>{props.item.releaseDate}</h4>
+            </div>
+        )
+    }
 
-  const simpleView=()=>{
-    return(
-        <div style={simpleStyle}>
-            <h3>{props.item.trackName}</h3>
-            <h4>{props.item.collectionName}</h4>
-        </div>
-    )
-  }
-  const detailView=()=>{
-    return(
-        <div style={detailStyle}>
-            <h2>{props.item.trackName}</h2>
-            <h3>{props.item.collectionName}</h3>
-            <h4>{props.item.primaryGenreName}</h4>
-            <h4>{props.item.releaseDate}</h4>
-        </div>
-    )
-  }
     return (
-    <div onClick={()=>setView(!view)} style={{'display':'inline-block'}}>
-       {view? detailView(): simpleView()}
-    </div>
-  )
-}
+        <div onClick={() => setView(!view)}
+        style={{'display': 'inline-block'}}>
+            {view ? detailView() : simpleView()}
+        </div>
+    )
+} 
 
 export default GalleryItem
